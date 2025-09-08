accaf8114c26468700c07dcf3569249c0bac19a9
MihlaliKota <hlalikota@gmail.com>
2025-09-08 23:34:49 +0200

    Finished Day-2

---
+++ Day-2/notes.md
@@ -0,0 +1,7 @@
+# Day 2 
+
+## What the exercise asked
+The exercise asked me to create three programs and one of them was just a follow along.
+
+## What worked and what didn't
+I had an error I couldn't stop encountering. Until I went to Claude AI. I then saw that my mistake was given a correction within the error popup. I love rust!
--- Day-2/variables-and-datatypes/src/main.rs
+++ Day-2/variables-and-datatypes/src/main.rs
@@ -1,14 +1,15 @@
 fn main() {
-    let name = "Mihlali";
-    let mut age = 26;
-    age += 1;
-    let favorite_number = 3.14;
+    let my_integer = 5;
+    let my_float = 3.14;
     let is_learning_rust = true;
-    let initial = 'M';
+    let favorite_letter = 'M';
+    let my_scores: [i32; 5] = [58, 65, 43, 24, 92];
+    let hobby = "reading manhwa.";
 
-    println!("Name: {}", name);
-    println!("Updated Age: {}", age);
-    println!("Favorite Number: {}", favorite_number);
-    println!("Learning Rust: {}", is_learning_rust);
-    println!("Initial: {}", initial);
+    println!("Favorite number: {}", my_integer);
+    println!("Second favorite number: {}", my_float);
+    println!("Is he learning rust?: {}", is_learning_rust);
+    println!("Favorite letter: {}", favorite_letter);
+    println!("My scores: {:?}", my_scores);
+    println!("I enjoy {}", hobby);
 }
